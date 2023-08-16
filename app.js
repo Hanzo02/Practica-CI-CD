56613dc51178aabb3eadf7fa80ee2f04f729cb31
20212216 <20212216@itla.edu.do>
2023-08-16 00:33:17 -0400

    subiendo

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hellouda, haciendo pruebas'); 
+    res.send('Hellouda, haciendo pruebas!'); 
 });
 
 // Escucha en un puerto específico
--- test/test.js
+++ test/test.js
@@ -11,7 +11,7 @@ describe('Pruebas de la aplicación web', () => {
             .get('/')
             .end((err, res) => {
                 expect(res).to.have.status(200);
-                expect(res.text).to.equal('Hellouda, haciendo pruebas');
+                expect(res.text).to.equal('Hellouda, haciendo pruebas!');
                 done();
             });
     });
